f4e6846111cf3cb65bf30f4d45923895cecc57c8
ChangHB <hbc443@gmail.com>
2021-07-20 23:55:54 +0900

    day7

---
+++ day7/basic006.py
@@ -0,0 +1,15 @@
+#정수 2개 입력받아 비교하기1
+a, b = input().split()
+print(a<b)
+
+#정수 2개 입력받아 비교하기2
+a, b = input().split()
+print(a==b)
+
+#정수 2개 입력받아 비교하기3
+a, b = input().split()
+print(a<=b)
+
+#정수 2개 입력받아 비교하기4
+a, b = input().split()
+print(a!=b)
